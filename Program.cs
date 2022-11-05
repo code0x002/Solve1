f01c74fd9b562db1f2b5fd8bf9f57eeb6efca4c1
code0x002 <47268540+code0x002@users.noreply.github.com>
2022-11-06 00:26:26 +0700

    Solve1 v.2 -add unique random

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SolveApp1
@@ -8,36 +9,53 @@ namespace SolveApp1
         static void Main(string[] args)
         {
             int[] numbers = new int[] { 8, 4, 2, 6, 5 };
-            string[] operators = new string[] { "+", "-", "*", "/", "%", "^" }; 
+            string[] operators = new string[] { "+", "-", "*", "/", "%", "^" };
+
+            List<string> storageNumbersOperatorSet = new List<string>();
 
             int idx = 1;
+            int ar = 1;
             while (true)
             {
                 Random rnd = new Random();
                 int[] numbersShuffle = numbers.OrderBy(x => rnd.Next()).ToArray();
+                int[] randomOperatorSet = new int[] { rnd.Next(0, 6), rnd.Next(0, 6), rnd.Next(0, 6), rnd.Next(0, 6) };
 
-                string solve = "";
-                double result = 0;
+                string numbersOperatorSet = numbersShuffle[0].ToString();
                 for (int i = 1; i < numbersShuffle.Length; i++)
                 {
-                    int rndOperator = rnd.Next(0, 6);
-
-                    result = Operator(operators[rndOperator], (i == 1 ? (double)numbersShuffle[i - 1] : result), numbersShuffle[i]);
-
-                    solve = string.Format(@"{0} {1} {2}", (i == 1 ? numbersShuffle[i - 1].ToString() : solve), operators[rndOperator], numbersShuffle[i]);
+                    numbersOperatorSet += string.Format(@"{0}{1}", operators[randomOperatorSet[i - 1]], numbersShuffle[i]);
                 }
 
-                if ((int)result == 497)
+                if (!storageNumbersOperatorSet.Exists(x => x == numbersOperatorSet))
                 {
-                    Console.WriteLine("[Random: " + idx + "] " + solve + " = " + (int)result + " = 497");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("[Random: " + idx + "] " + solve + " = " + (int)result + " != 497");
+                    storageNumbersOperatorSet.Add(string.Join("", numbersOperatorSet));
+
+                    string solve = "";
+                    double result = 0;
+                    for (int i = 1; i < numbersShuffle.Length; i++)
+                    {
+                        int rndOperator = randomOperatorSet[i - 1];
+
+                        result = Operator(operators[rndOperator], (i == 1 ? (double)numbersShuffle[i - 1] : result), numbersShuffle[i]);
+
+                        solve = string.Format(@"{0} {1} {2}", (i == 1 ? numbersShuffle[i - 1].ToString() : solve), operators[rndOperator], numbersShuffle[i]);
+                    }
+
+                    if ((int)Math.Abs(result) == 497)
+                    {
+                        Console.WriteLine("[Random: " + idx + ", ARandom: " + ar + "] " + solve + " = " + (int)result + " = 497");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("[Random: " + idx + ", ARandom: " + ar + "] " + solve + " = " + (int)result + " != 497");
+                    }
+
+                    idx++;
                 }
 
-                idx++;
+                ar++;
             }
 
             Console.ReadKey();
